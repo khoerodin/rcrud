02e2927d8cc4d680c7a7d6531f5e6cc2f63e3291
Khoerodin <khoerodin@live.com>
2018-03-12 06:26:19 +0700

    sample validation pada create new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,16 +3,23 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def create
-    @article = Article.new(article_params)
- 
-    @article.save
-    redirect_to @article
-  end
-
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end 
 
   private
     def article_params
